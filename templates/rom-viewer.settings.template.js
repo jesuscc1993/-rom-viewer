c8e992e51d60f02205f2df222737a8b8051b5940
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2022-08-02 21:39:49 +0200

    Removed optional properties from templates.

--- templates/rom-viewer.settings.template.js
+++ templates/rom-viewer.settings.template.js
@@ -5,67 +5,54 @@ const romSettings = {
   platforms: {
     PS1: {
       emulatorPath: 'DuckStation/duckstation-qt-x64-ReleaseLTCG.exe',
-      romPath: 'PS1',
       roms: [],
     },
     PS2: {
       emulatorPath: 'PCSX2/pcsx2-qtx64.exe',
-      romPath: 'PS2',
       roms: [],
     },
     PS3: {
       emulatorPath: 'RPCS3/rpcs3.exe',
-      romPath: 'PS3',
       roms: [],
     },
     PSP: {
       emulatorPath: 'PPSSPP/PPSSPPWindows64.exe',
-      romPath: 'PSP',
       roms: [],
     },
     Xbox: {
       emulatorPath: 'Xemu/xemu.exe',
-      romPath: 'Xbox',
       roms: [],
     },
     X360: {
       emulatorPath: 'Xenia/xenia_canary.exe',
-      romPath: 'X360',
       roms: [],
     },
     SNES: {
       emulatorPath: 'bSNES/bsnes_hd.exe',
-      romPath: 'SNES',
       roms: [],
     },
     GBA: {
       emulatorPath: 'mGBA/mGBA.exe',
-      romPath: 'GBA',
       roms: [],
     },
     NDS: {
       emulatorPath: 'DesMuMe/DeSmuME-VS2019-x64-Release.exe',
-      romPath: 'NDS',
       roms: [],
     },
     '3DS': {
       emulatorPath: 'DesMuMe/DeSmuME-VS2019-x64-Release.exe',
-      romPath: '3DS',
       roms: [],
     },
     Wii: {
       emulatorPath: 'Dolphin/Dolphin.exe',
-      romPath: 'Wii',
       roms: [],
     },
     'Wii U': {
       emulatorPath: 'Cemu/Cemu.exe',
-      romPath: 'Wii U',
       roms: [],
     },
     Switch: {
       emulatorPath: 'yuzu/yuzu-windows-msvc/yuzu.exe',
-      coverPath: 'Switch',
       romPath: 'Switch/nsp',
       roms: [],
     },
